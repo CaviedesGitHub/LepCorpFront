b88052d9a8eb79dee13840c65e8fa4424037d29a
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2025-01-28 20:53:36 -0500

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'lepcorpsas1';
+
+  ngOnInit(): void {
+    $(document).ready(function(){
+      $("#btnAbrir").on('click', function(){
+        alert("Mensaje desde el boton");
+      })
+    });
+  }
 }
